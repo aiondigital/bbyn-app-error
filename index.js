899ba0bd8c25932f8c7fc2531c85f0892366febf
Muhammad Zaid <mzaid@aiondigital.com>
2020-08-13 16:07:13 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const errors = require("./errors.json");
+class AppError extends Error {
+  constructor(type = "SERVER_ERROR", params) {
+    super(params);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, AppError);
+    }
+    if (errors[type]) {
+      this.name = errors[type].name;
+      this.description =
+        params && params.message
+          ? { en: params["message"], ar: params["message"] }
+          : errors[type].description;
+      this.code = errors[type].code;
+      this.status = errors[type].status;
+    } else {
+      this.name = type;
+      this.code = type;
+      this.status = 200;
+      this.description =
+        params && params.message ? { en: params["message"], ar: "" } : {};
+    }
+    this.date = new Date().toString();
+  }
+}
+module.exports = AppError;
